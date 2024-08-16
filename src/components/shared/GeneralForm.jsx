a4a73ae5738e5302d7a482854c407aa58e4a795c
Lionel <lpei1900@gmail.com>
2024-08-16 16:49:16 -0400

    Created the buttons for the forms

--- src/components/shared/GeneralForm.jsx
+++ src/components/shared/GeneralForm.jsx
@@ -2,25 +2,46 @@ import React from 'react';
 import { formConfig } from '../../formConfig.js';
 
 const GenerateForm = ({section}) => {
-    return (
-      <form>
-        {formConfig[section].map((field) => (
-          <div key={field.name}>
-            <label htmlFor={field.name}>{field.placeholder}</label>
-            {field.type === "textarea" ? (
-              <textarea
-                placeholder={field.placeholder}
-              />
-            ) : (
-              <input
-                type={field.type}
-                placeholder={field.placeholder}
-              />
-            )}
-          </div>
-        ))}
-      </form>
-    );
+
+
+  const handleClear = () => {
+  };
+
+  const handleCancel = () => {
+
+  };
+
+  const handleSave = () => {
+
+  };
+
+
+
+  return (
+    <form>
+      {formConfig[section].map((field) => (
+        <div key={field.name}>
+          <label htmlFor={field.name}>{field.placeholder}</label>
+          {field.type === "textarea" ? (
+            <textarea
+              placeholder={field.placeholder}
+            />
+          ) : (
+            <input
+              type={field.type}
+              placeholder={field.placeholder}
+            />
+          )}
+        </div>
+      ))}
+      <div>
+        <button type="button" onClick={handleClear}>Clear</button>
+        <button type="button" onClick={handleCancel}>Cancel</button>
+        <button type="submit" onClick={handleSave}>Save</button>
+      </div>
+
+    </form>
+  );
 };
 
 export default GenerateForm;
