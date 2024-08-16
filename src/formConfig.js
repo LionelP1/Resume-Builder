14f995c2b61ec8d2b61083916b4a6fcb2f37dfb8
Lionel <lpei1900@gmail.com>
2024-08-15 23:25:58 -0400

    Created a configuration file for the forums

---
+++ src/formConfig.js
@@ -0,0 +1,28 @@
+export const formConfigs = {
+    information: [
+        { name: "fullName", type: "text", placeholder: "Job Title" },
+        { name: "email", type: "text", placeholder: "Company Name" },
+        { name: "phoneNumber", type: "text", placeholder: "Start Date" },
+        { name: "location", type: "text", placeholder: "End Date" },
+      ],
+
+    education: [
+        { name: "schoolName", type: "text", placeholder: "School Name" },
+        { name: "degree", type: "text", placeholder: "Degree" },
+        { name: "startDate", type: "text", placeholder: "Start Date" },
+        { name: "endDate", type: "text", placeholder: "End Date" },
+        ],
+
+    skills: [
+        {name: "skillName", type: "text", placeholder: "Skill" },
+    ],
+    
+    experience: [
+      { name: "jobTitle", type: "text", placeholder: "Job Title" },
+      { name: "companyName", type: "text", placeholder: "Company Name" },
+      { name: "startDate", type: "text", placeholder: "Start Date" },
+      { name: "endDate", type: "text", placeholder: "End Date" },
+      { name: "description", type: "textarea", placeholder: "Description" },
+    ],
+  };
+  
