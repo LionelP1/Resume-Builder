0cfa6f7210dc52bbf3ae2325665f9cdecdeed6b8
Lionel <lpei1900@gmail.com>
2024-08-18 00:37:06 -0400

    Deleted unneeded files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import ExperienceSection from './components/experience/ExperienceSection.jsx';
-
+import ResumeSection from './components/shared/ResumeSection.jsx';
 
 const App = () => {
   return (
     <div>
-      <ExperienceSection />
+      <ResumeSection section="information" ></ResumeSection>
+      <ResumeSection section="experience" ></ResumeSection>
+      <ResumeSection section="education" ></ResumeSection>
     </div>
   );
 };
